6cc8a75eec0bf68c7aff6343de5b7fbab2859fb0
Carlos romero <soportecarlosromero@gmail.com>
2025-06-12 08:31:50 -0500

    initializer + gitignore

---
+++ src/main/java/com/romerojdev/infinito/pos_ai/PosAiApplication.java
@@ -0,0 +1,13 @@
+package com.romerojdev.infinito.pos_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romerojdev/infinito/pos_ai/PosAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romerojdev.infinito.pos_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
